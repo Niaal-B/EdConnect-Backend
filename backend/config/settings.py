6e00a1590510a4d3a32b92875617ff2404b9d8aa
Niaal-B <nihnihal504@gmail.com>
2025-09-14 18:20:53 +0530

    fix: added back the cors allowed orgins in the settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -85,12 +85,11 @@ ASGI_APPLICATION = 'config.asgi.application'
 
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "https://ed-connect-frontend.vercel.app"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://ed-connect-frontend.vercel.app"
+]
 
-CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 

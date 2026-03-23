e1ce8bc7110f4e1aff0a05544ea6bb45b0ec3a75
Niaal-B <nihnihal504@gmail.com>
2026-03-23 15:42:25 +0530

    fix: remove non-existent celery task

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -13,8 +13,4 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'clear-redis-cache': {
-        'task': 'api.tasks.clear_expired_cache',  
-        'schedule': timedelta(minutes=60),
-    },
 }
